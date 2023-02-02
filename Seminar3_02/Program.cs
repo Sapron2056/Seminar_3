24f676b7438673ce0e843bfb8b453b5c1fcc815b
Ivan <ivanka322@gmail>
2023-02-02 21:31:25 +0300

    решение 4 задачи

--- Seminar3_02/Program.cs
+++ Seminar3_02/Program.cs
@@ -8,23 +8,44 @@ Console.Clear();
  int ch1 = int.Parse(Console.ReadLine()!);
 
 
- if (ch1 == 1)
- {
-    Console.Write("X>0 , Y>0 ");
- }
- else if (ch1 == 2)
- {
-    Console.Write("X<0 , Y>0 ");
- }
- else if (ch1 == 3 )
- {
-    Console.Write("X<0 , Y<0 ");
- }
- else if (ch1 == 4 )
- {
-    Console.Write("X>0 , Y<0 ");
- }
-else 
+//  if (ch1 == 1)
+//  {
+//     Console.Write("X>0 , Y>0 ");
+//  }
+//  else if (ch1 == 2)
+//  {
+//     Console.Write("X<0 , Y>0 ");
+//  }
+//  else if (ch1 == 3 )
+//  {
+//     Console.Write("X<0 , Y<0 ");
+//  }
+//  else if (ch1 == 4 )
+//  {
+//     Console.Write("X>0 , Y<0 ");
+//  }
+// else 
+// {
+//     Console.Write("Ввкдите число от 1 до 4 ");
+// }
+
+switch (ch1)
 {
-    Console.Write("Ввкдите число от 1 до 4 ");
+    case 1:
+        Console.Write("X>0 , Y>0 ");
+        break;
+
+    case 2:
+        Console.Write("X<0 , Y>0 ");
+        break;
+    case 3:
+        Console.Write("X<0 , Y<0 ");
+        break;
+    case 4:
+        Console.Write("X<0 , Y>0 ");
+        break;
+    default:
+        Console.Write("Ввкдите число от 1 до 4 ");
+        break;
+
 }
